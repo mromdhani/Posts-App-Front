4167846d97eba00f5ff32defa59811054a434bba
Mohamed Romdhani (INSAT) <mromdhani7@gmail.com>
2018-05-30 11:17:18 +0100

    Commit initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule   // Since Angular 4.3 +
+   // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
